7a65b9c83f08ca4271a3996423e7df65b29d5188
ademolac <seiduademola03@gmail.com>
2022-08-27 02:53:58 +0100

    My change

---
+++ hava.js
@@ -0,0 +1,57 @@
+    // let box=document.getElementById('box');
+    // let head=document.getElementById('head'); 
+//     let arrs=[{name:'Ibrahim',age:20},{name:'Josh',age:23},{name:'Bjay',age:24},{name:'Blessing',age:25},{name:'AZ',age:29},{name:'Ade',age:25},{name:'KB',age:27}]
+    
+//    function find(){
+//       head.innerHTML=arrs[box.value].name
+//       head1.innerHTML=arrs[box.value].age
+//     }
+
+    //  let human={name:"Teslim",age:10,eye:2,
+    //  walk:function(){
+    //      this.legs==2 ? log('Now walking') :log(`can't walk1`)
+    //  }
+    //  .legs=1}
+    //  console.log(
+    //      human.walk() 
+    //  )
+//    function find(){
+  
+//       head.innerHTML=numbas.splice(4,2)(box.value,box1.value)
+      
+//     }
+    //  for (let i = 5; i <=25; i++) {
+    //     console.log(i)    
+    //  }
+    
+    //  for (let i = 20; i >= 1 ; i--) {
+    //     console.log(i)    
+    //  }
+
+    // head.=numbas
+    // b= box.innerHTML
+    // display.innerHTML=numbas
+
+
+    
+    //  for (let i=1; i <=12; i++) {
+    //     display.innerHTML += `<p >2 * ${i} = ${2*i} </p>`    
+    //  }
+// for (let index = 2; index <=12; index++){
+//     for (let i = 1; i <=12; i++) {
+//         head.innerHTML+= `<p>${index} * ${i} = ${index*i}</p>`
+        
+//     }
+// }
+//  <textarea id="box1"></textarea><button class="btn btn-sm bg-danger">PUSH</button><br> 
+//  <button onclick="find()" id="mem">FIND</button> 
+// function show(){
+    // head.innerHTML = '';
+//     for (let index = 0; index < arrs.length; index++) {
+//      head.innerHTML+= `${index + 1}. ${arrs[index]} <button  onclick="del(${index})" >delete</button> <br>`;
+//     }
+// }
+
+<button type="button"
+onclick="document.getElementById('id1').style.color = 'red'">
+Click Me!</button>
